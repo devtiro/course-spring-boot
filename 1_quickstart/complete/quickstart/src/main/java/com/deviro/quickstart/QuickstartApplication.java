031d97c6c240c127795e2668d001024da29d817a
Aaron Bourne <aarondbourne@gmail.com>
2023-06-15 11:03:54 +0100

    Added quickstart app complete.

---
+++ 1_quickstart/complete/quickstart/src/main/java/com/deviro/quickstart/QuickstartApplication.java
@@ -0,0 +1,13 @@
+package com.deviro.quickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickstartApplication.class, args);
+	}
+
+}
---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Build all projects
+# For information on why Maven multi-module wasn't used, please see README.md
+
+# Fail on errors
+set -e
+
+projects=( "1_quickstart/complete/quickstart" )
+
+# Build all projects
+for project in "${projects[@]}"; do
+    cd "./${project}"
+    . mvnw clean verify
+    cd -
+done
